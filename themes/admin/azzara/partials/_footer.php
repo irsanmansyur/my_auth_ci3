829dcdc6bd8ce6ba2be2a17daf617bd4f23ba197
tantangin <irsan00mansyur@gmail.com>
2020-10-30 01:36:42 +0800

    set Css

---
+++ themes/admin/azzara/partials/_footer.php
@@ -0,0 +1,1 @@
+<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
--- themes/admin/azzara/partials/_js_files.php
+++ themes/admin/azzara/partials/_js_files.php
@@ -19,35 +19,35 @@
 
 
 <script>
-   function ready(callbackFunc) {
-      if (document.readyState !== "loading") {
-         callbackFunc();
-      } else if (document.addEventListener) {
-         document.addEventListener("DOMContentloaded", callbackFunc());
-      } else
-         document.attachEvent('onreadystatechange', function() {
-            if (document.readyState == 'complete')
-               callbackFunc();
-         })
-   }
+  function ready(callbackFunc) {
+    if (document.readyState !== "loading") {
+      callbackFunc();
+    } else if (document.addEventListener) {
+      document.addEventListener("DOMContentloaded", callbackFunc());
+    } else
+      document.attachEvent('onreadystatechange', function() {
+        if (document.readyState == 'complete')
+          callbackFunc();
+      })
+  }
 
-   const baseUrl = "<?= base_url() ?>";
+  const baseUrl = "<?= base_url() ?>";
 
-   ready(() => {
-      let path = window.location.href;
-      path = path.indexOf("#") > 0 ? path.substring(0, path.indexOf("#")) : path;
-      path = path.indexOf("?") > 0 ? path.substring(0, path.indexOf("?")) : path;
-      path.indexOf("index.php") > 0 ? [console.log("Ada  index"), path = path.replace("/index.php", "")] : console.log("tdk ada index");
-      path = path.toLowerCase().toString();
+  ready(() => {
+    let path = window.location.href;
+    path = path.indexOf("#") > 0 ? path.substring(0, path.indexOf("#")) : path;
+    path = path.indexOf("?") > 0 ? path.substring(0, path.indexOf("?")) : path;
+    path.indexOf("index.php") > 0 ? [console.log("Ada  index"), path = path.replace("/index.php", "")] : console.log("tdk ada index");
+    path = path.toLowerCase().toString();
 
-      let elLinkSubmenu = document.querySelector(`li.s-menu>a[href="${path}"]`);
+    let elLinkSubmenu = document.querySelector(`li.s-menu>a[href="${path}"]`);
 
-      if (elLinkSubmenu) {
-         elLinkSubmenu.closest("li").classList.add("active");
-         elLinkSubmenu.closest(".collapse").classList.add("show");
-         elLinkSubmenu.closest(".nav-item").classList.add('active', 'submenu');
-      }
-   })
+    if (elLinkSubmenu) {
+      elLinkSubmenu.closest("li").classList.add("active");
+      elLinkSubmenu.closest(".collapse").classList.add("show");
+      elLinkSubmenu.closest(".nav-item").classList.add('active', 'submenu');
+    }
+  })
 
-   $('#basic-datatables').DataTable();
+  $('#basic-datatables').DataTable();
 </script>
