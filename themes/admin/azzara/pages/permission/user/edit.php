fca9f87dbd63e476c9dd80b4835b7e8755852e9c
tantangin <irsan00mansyur@gmail.com>
2021-01-26 23:35:54 +0800

    ok

--- themes/admin/azzara/pages/permission/user/edit.php
+++ themes/admin/azzara/pages/permission/user/edit.php
@@ -56,21 +56,8 @@
               </div>
             </div>
             <div class="col-md-4">
-              <div class="card card-profile card-secondary">
-                <div class="card-header" style="background-image: url('<?= $thema_folder; ?>assets/img/blogpost.jpg')">
-                  <div class="profile-picture">
-                    <div class="avatar avatar-xl">
-                      <img src="<?= base_url('assets/img/profile/' . $user->profile) ?>" alt="..." class="avatar-img rounded-circle">
-                    </div>
-                  </div>
-                </div>
-                <div class="card-body">
-                  <div class="user-profile text-center">
-                    <div class="name"><?= $user->nama_user; ?>,</div>
-                    <div class="email"><?= $user->email; ?></div>
-                  </div>
-                </div>
-              </div>
+              <?php $this->load->view($thema_load . "partials/_card-profile.php"); ?>
+
             </div>
           </div>
         </div>
@@ -78,35 +65,8 @@
     </div>
 
     <!-- Custom template | don't include it in your project! -->
-    <div class="custom-template">
-      <div class="title">Settings</div>
-      <div class="custom-content">
-        <div class="switcher">
-          <div class="switch-block">
-            <h4>Topbar</h4>
-            <div class="btnSwitch">
-              <button type="button" class="changeMainHeaderColor" data-color="blue"></button>
-              <button type="button" class="selected changeMainHeaderColor" data-color="purple"></button>
-              <button type="button" class="changeMainHeaderColor" data-color="light-blue"></button>
-              <button type="button" class="changeMainHeaderColor" data-color="green"></button>
-              <button type="button" class="changeMainHeaderColor" data-color="orange"></button>
-              <button type="button" class="changeMainHeaderColor" data-color="red"></button>
-            </div>
-          </div>
-          <div class="switch-block">
-            <h4>Background</h4>
-            <div class="btnSwitch">
-              <button type="button" class="changeBackgroundColor" data-color="bg2"></button>
-              <button type="button" class="changeBackgroundColor selected" data-color="bg1"></button>
-              <button type="button" class="changeBackgroundColor" data-color="bg3"></button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="custom-toggle">
-        <i class="flaticon-settings"></i>
-      </div>
-    </div>
+    <?php $this->load->view($thema_load . "partials/_custom_templates.php"); ?>
+
     <!-- End Custom template -->
   </div>
   <?php $this->load->view($thema_load . "partials/_js_files.php"); ?>
